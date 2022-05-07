2b5dee26ce697e974e2adf7a28c47bb0962e85bf
Sergey <perfer79@gmail.com>
2022-05-07 11:41:54 +0700

    Задача 8

---
+++ task8_even_1_to_N/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает на вход число N, на выходе показывает все четные до N]
+
+Console.WriteLine("Введите число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+int Temp = Number - 1;
+for (int i = 2; i <= Number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        if (i < Temp)
+        {
+            Console.Write(", ");
+        }
+    }
+}
